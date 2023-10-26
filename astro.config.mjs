f3f9be7fe4495880a1e451bdd4c71a9a77e5a2b9
ami_AVK <alexkulickov@gmail.com>
2023-10-26 16:05:39 +0300

    Fix font bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+    })
+  ],
   site: 'https://ami-AVK.github.io',
   base: '/santech-portal',
 });
