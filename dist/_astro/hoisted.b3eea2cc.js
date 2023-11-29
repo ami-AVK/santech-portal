c3f8a8bc03b819ea1ababf5c21c6020d870bf1fe
ami_AVK <alexkulickov@gmail.com>
2023-11-29 09:22:39 +0300

    Описания на компоненты Аппбар скрывается только на главной

--- dist/_astro/hoisted.b3eea2cc.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.dc0355b4.js";import{c as a}from"./fancybox.8bb8286b.js";import"./hoisted.17158c12.js";a.bind("[data-fancybox]",{Thumbs:{type:"classic"}});
---
+++ dist/_astro/hoisted.f070eb50.js
@@ -0,0 +1,1 @@
+import"./hoisted.3d675e1e.js";import{c as a}from"./fancybox.8bb8286b.js";import"./hoisted.17158c12.js";a.bind("[data-fancybox]",{Thumbs:{type:"classic"}});
