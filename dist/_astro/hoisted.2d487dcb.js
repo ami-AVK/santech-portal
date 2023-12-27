af347cc2db2ab1eec4ed9d165f8f58ac79d5a478
ami_AVK <alexkulickov@gmail.com>
2023-12-27 17:37:33 +0300

    Полностью вытащил сайдбар из шапки Отключил ViewTransition

---
+++ dist/_astro/hoisted.2d487dcb.js
@@ -0,0 +1,1 @@
+import"./Search.astro_astro_type_script_index_0_lang.31f1e7c0.js";import{c as a}from"./fancybox.8bb8286b.js";a.bind("[data-fancybox]",{Thumbs:{type:"classic"}});
--- dist/_astro/hoisted.a02d0b7a.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.6f97490d.js";import{c as a}from"./fancybox.8bb8286b.js";a.bind("[data-fancybox]",{Thumbs:{type:"classic"}});
